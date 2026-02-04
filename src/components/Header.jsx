7942670be9876db466481134228ed262e4cd9c5d
max1maln0 <max.shipilov2008@gmail.com>
2026-02-04 18:31:11 +0300

    style: fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     return(
-        <header className='sticky top-0 z-50 flex justify-between items-center p-4 bg-[#eeeef0]/15 border-b border-gray-300 rounded-b-xl'>
+        <header className='sticky top-0 z-50 flex justify-between items-center p-4 bg-[#eeeef0]/75 border-b border-gray-300 rounded-b-xl'>
 
             <div className='flex items-center gap-1'>
                 <img src={Logo} alt="Logo" className='w-8 h-8 object-contain -translate-y-[3px]'/>
@@ -13,7 +13,7 @@ export default function Header() {
             </div>
             
 
-            <nav className='flex items-center gap-6 text-sm font-medium text-gray-500'>
+            <nav className='flex items-center gap-6 text-sm font-medium text-gray-600'>
                 <Link to="/" className='relative pb-1 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-full after:scale-x-0 after:origin-left after:bg-black after:transition-transform after:duration-300 after:ease-out hover:after:scale-x-100 focus-visible:after:scale-x-100 active:after:scale-x-100 hover:text-gray-800'>Главная</Link>
                 <Link to="/catalog" className='relative pb-1 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-full after:scale-x-0 after:origin-left after:bg-black after:transition-transform after:duration-300 after:ease-out hover:after:scale-x-100 focus-visible:after:scale-x-100 active:after:scale-x-100 hover:text-gray-800'>Каталог</Link>
                 <Link to="/about" className='relative pb-1 after:absolute after:left-0 after:-bottom-1 after:h-[2px] after:w-full after:scale-x-0 after:origin-left after:bg-black after:transition-transform after:duration-300 after:ease-out hover:after:scale-x-100 focus-visible:after:scale-x-100 active:after:scale-x-100 hover:text-gray-800'>О нас</Link>

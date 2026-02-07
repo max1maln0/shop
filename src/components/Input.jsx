bbc1d94ea771d8aa762efbc021c5b30004ed8f3a
max1maln0 <max.shipilov2008@gmail.com>
2026-02-07 14:52:13 +0300

    feat: add search logic

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,17 @@
 
 import { Search } from 'lucide-react';
 
-export default function Input() {
+export default function Input({ value, onChange }) {
     return(
         <div className='border-2 border-gray-400/30 flex gap-3 p-2 w-full items-center focus-within:border-blue-500'>
             <Search className='h-5 w-5'/>
-            <input type="text" placeholder='Поиск товаров...' className='w-full outline-none focus:border-blue-600'/>
+            <input type="text"
+             placeholder='Поиск товаров...' 
+             className='w-full outline-none focus:border-blue-600'
+             value={value}
+             onChange={(e) => onChange(e.target.value)}
+             
+             />
         </div>
     )
 }
--- src/pages/sections/CatalogSection.jsx
+++ src/pages/sections/CatalogSection.jsx
@@ -2,8 +2,13 @@ import Input from "@components/Input";
 import { products } from "@data/products";
 import Card from "@components/ui/Card";
 import SideBar from "@components/SideBar";
+import { useState } from "react";
 
 export default function CatalogSection() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const filteredProducts = products.filter((item) => item.title.toLowerCase().includes(searchQuery.toLowerCase()));
+
     return (
         <section className="m-5 flex flex-col w-full max-w-7xl mx-auto">
             <h1 className="section-title">Каталог товаров</h1>
@@ -12,11 +17,15 @@ export default function CatalogSection() {
                 <SideBar />
 
                 <div className="flex flex-col items-start gap-5 ">
-                    <Input />
+                    <Input value={searchQuery} onChange={setSearchQuery}/>
                     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                        {products.map((item) => (
-                        <Card key={item.id} product={item} />
-                        ))}
+                        {filteredProducts.length > 0 ? (
+                            filteredProducts.map((item) => (
+                                <Card key={item.id} product={item} />
+                            ))
+                        ) : (
+                            <p className="text-gray-500">Товары не найдены</p>
+                        )}
                     </div>
                 </div>
             </div>

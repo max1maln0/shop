e6040df718b5bf6fa0851f2f180e191e6d2e4739
max1maln0 <max.shipilov2008@gmail.com>
2026-02-07 22:34:22 +0300

    feat: add router in categories card

--- src/pages/sections/Categories.jsx
+++ src/pages/sections/Categories.jsx
@@ -5,6 +5,7 @@ import Watch from "@assets/categories/watch.svg";
 import Plug from "@assets/categories/plug.svg";
 import Button from "@components/Button";
 import Arrow from "@assets/right-arrow-svgrepo-com.svg";
+import { Link } from "react-router-dom";
 
 
 export default function Categories() {
@@ -16,33 +17,41 @@ export default function Categories() {
 
             
             <div className="grid grid-cols-1 gap-5 m-6 p-3 sm:grid-cols-2 md:grid-cols-4">
-                <Card variant="default">
-                    <img src={Phone} alt="phone" className="h-30 w-30"/>
-                    <h3 className="text-[1.25rem] font-medium">Смартфоны</h3>
-                    <p>Флагманы и хиты продаж.</p>
-                    <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
-                </Card>
+                <Link to="/catalog">
+                    <Card variant="default">
+                        <img src={Phone} alt="phone" className="h-30 w-30"/>
+                        <h3 className="text-[1.25rem] font-medium">Смартфоны</h3>
+                        <p>Флагманы и хиты продаж.</p>
+                        <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
+                    </Card>
+                </Link>
 
-                <Card variant="default">
-                    <img src={Pods} alt="pods" className="h-30 w-30"/>
-                    <h3 className="text-[1.25rem] font-medium">Наушники</h3>
-                    <p>Беспроводной звук высокого качества.</p>
-                    <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
-                </Card>
+                <Link to="/catalog">
+                    <Card variant="default">
+                        <img src={Pods} alt="pods" className="h-30 w-30"/>
+                        <h3 className="text-[1.25rem] font-medium">Наушники</h3>
+                        <p>Беспроводной звук высокого качества.</p>
+                        <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
+                    </Card>
+                </Link>
 
-                <Card variant="default">
-                    <img src={Watch} alt="watch" className="h-30 w-30"/>
-                    <h3 className="text-[1.25rem] font-medium">Умные часы</h3>
-                    <p>Умные гаджеты.</p>
-                    <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
-                </Card>
+                <Link to="/catalog">
+                    <Card variant="default">
+                        <img src={Watch} alt="watch" className="h-30 w-30"/>
+                        <h3 className="text-[1.25rem] font-medium">Умные часы</h3>
+                        <p>Умные гаджеты.</p>
+                        <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
+                    </Card>
+                </Link>
 
-                <Card variant="default">
-                    <img src={Plug} alt="plug" className="h-30 w-30"/>
-                    <h3 className="text-[1.25rem] font-medium">Аксессуары</h3>
-                    <p>Все для ваших устройств.</p>
-                    <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
-                </Card>
+                <Link to="/catalog">
+                    <Card variant="default">
+                        <img src={Plug} alt="plug" className="h-30 w-30"/>
+                        <h3 className="text-[1.25rem] font-medium">Аксессуары</h3>
+                        <p>Все для ваших устройств.</p>
+                        <Button variant="look">Смотреть <img src={Arrow} alt="arrow" className="h-5 w-5"/></Button>
+                    </Card>
+                </Link>
             </div>
         </section>
         </>
